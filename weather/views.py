505cb761c9c405ec00cb10a693549d3f6a726c02
peter polle <peter.m.polle@gmail.com>
2018-07-02 22:00:39 +0300

    this commit will update static renderin of weather api data onto the application

--- weather/views.py
+++ weather/views.py
@@ -1,8 +1,24 @@
 from django.shortcuts import render
+from django.conf import settings
+import requests
 
 # Create your views here.
 def index(request):
 	'''
 	View function to render the index page
 	'''
-	return render(request,'index/index.html')
+	endpoint = 'http://api.openweathermap.org/data/2.5/weather?q={city}&units=imperial&appid={weather_api}'
+	city = 'Las Vegas'
+	weather_api = settings.WEATHER_API
+	url = endpoint.format(city = city,weather_api = weather_api)
+
+	response = requests.get(url).json()
+
+	city_weather = {
+	'city':city,
+	'temp':response['main']['temp'],
+	'description':response['weather'][0]['description'],
+	'icon':response['weather'][0]['icon'],
+	}
+
+	return render(request,'index/index.html',{"city_weather":city_weather})
