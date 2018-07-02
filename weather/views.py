20fd333e21b35ef898ddbaf3ea2edf66b67eb0db
peter polle <peter.m.polle@gmail.com>
2018-07-02 22:09:27 +0300

    this commit will update hardcoded retrieval of weather data

--- weather/views.py
+++ weather/views.py
@@ -8,7 +8,7 @@ def index(request):
 	View function to render the index page
 	'''
 	endpoint = 'http://api.openweathermap.org/data/2.5/weather?q={city}&units=imperial&appid={weather_api}'
-	city = 'Las Vegas'
+	city = 'Kilifi'
 	weather_api = settings.WEATHER_API
 	url = endpoint.format(city = city,weather_api = weather_api)
 
