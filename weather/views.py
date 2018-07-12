cb2ddd3b540b315e4abf0a5efa54462b8a68738e
ppolle <peter.m.polle@gmail.com>
2018-07-12 22:54:53 +0300

    this commit will implement functionality to check whether a user is connected to the internet or not and render an appropriate message for it

--- weather/views.py
+++ weather/views.py
@@ -12,36 +12,40 @@ def index(request):
 	'''
 	View function to render the index page
 	'''
-	if request.GET.get("city"):
-		endpoint = 'http://api.openweathermap.org/data/2.5/weather?q={city}&units=imperial&appid={weather_api}'
-		city =request.GET.get("city")
-		weather_api = settings.WEATHER_API
-		url = endpoint.format(city = city,weather_api = weather_api)
+	try:
+		if request.GET.get("city"):
+			endpoint = 'http://api.openweathermap.org/data/2.5/weather?q={city}&units=imperial&appid={weather_api}'
+			city =request.GET.get("city")
+			weather_api = settings.WEATHER_API
+			url = endpoint.format(city = city,weather_api = weather_api)
 
-		response = requests.get(url).json()
+			response = requests.get(url).json()
 
-		city_weather = {
-		'city':city,
-		'temp':response['main']['temp'],
-		'description':response['weather'][0]['description'],
-		'icon':response['weather'][0]['icon'],
-		}
-		return render(request,'weather/index.html',{"city_weather":city_weather})
-	else:
-		endpoint = 'http://api.openweathermap.org/data/2.5/weather?q={city}&units=imperial&appid={weather_api}'
-		city ='Nairobi'
-		weather_api = settings.WEATHER_API
-		url = endpoint.format(city = city,weather_api = weather_api)
+			city_weather = {
+			'city':city,
+			'temp':response['main']['temp'],
+			'description':response['weather'][0]['description'],
+			'icon':response['weather'][0]['icon'],
+			}
+			return render(request,'weather/index.html',{"city_weather":city_weather})
+		else:
+			endpoint = 'http://api.openweathermap.org/data/2.5/weather?q={city}&units=imperial&appid={weather_api}'
+			city ='Nairobi'
+			weather_api = settings.WEATHER_API
+			url = endpoint.format(city = city,weather_api = weather_api)
 
-		response = requests.get(url).json()
+			response = requests.get(url).json()
 
-		city_weather = {
-		'city':city,
-		'temp':response['main']['temp'],
-		'description':response['weather'][0]['description'],
-		'icon':response['weather'][0]['icon'],
-		}
-		return render(request,'weather/index.html',{"city_weather":city_weather})
+			city_weather = {
+			'city':city,
+			'temp':response['main']['temp'],
+			'description':response['weather'][0]['description'],
+			'icon':response['weather'][0]['icon'],
+			}
+			return render(request,'weather/index.html',{"city_weather":city_weather})
+	except:
+		return render(request,'weather/error.html')
+
 
 def signup(request):
 	'''
